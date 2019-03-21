8a1fbf762ec4d5ff5b0184c57063ad3847811b45
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-21 10:48:53 +0100

    Testkategorie und DataRow

--- Taschenrechner/Taschenrechner.Tests/TaschenrechnerTests.cs
+++ Taschenrechner/Taschenrechner.Tests/TaschenrechnerTests.cs
@@ -8,6 +8,7 @@ namespace Taschenrechner.Tests
     {
         // Normalfall
         [TestMethod]
+        [TestCategory(Category.MSTest)]
         public void Taschenrechner_Add_5_and_3_results_8()
         {
             // Arrange
@@ -19,21 +20,43 @@ namespace Taschenrechner.Tests
             // Assert
             Assert.AreEqual(8, result);
         }
+        // Normalfälle zusammengefasst
+        [TestMethod]
+        [TestCategory(Category.MSTest)]
+        [DataRow(3,5,8)]
+        [DataRow(5,3,8)]
+        [DataRow(3,-5,-2)]
+        [DataRow(-3,5,2)]
+        [DataRow(0,0,0)]
+        public void Taschenrechner_Add(int z1, int z2,int expected)
+        {
+            // Arrange
+            var tr = new Taschenrechner();
+
+            // Act
+            var result = tr.Add(z1, z2);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
 
         // Extremfälle
         [TestMethod]
+        [TestCategory(Category.MSTest)]
         public void Taschenrechner_Add_MaxInt_and_1_throws_OverflowException()
         {
             var tr = new Taschenrechner();
             Assert.ThrowsException<OverflowException>(() => tr.Add(Int32.MaxValue, 1));
         }
         [TestMethod]
+        [TestCategory(Category.MSTest)]
         public void Taschenrechner_Add_MinInt_and_NEG1_throws_OverflowException()
         {
             var tr = new Taschenrechner();
             Assert.ThrowsException<OverflowException>(() => tr.Add(Int32.MinValue, -1));
         }
         [TestMethod]
+        [TestCategory(Category.MSTest)]
         public void Taschenrechner_Add_0_and_0_results_0()
         {
             var tr = new Taschenrechner();
