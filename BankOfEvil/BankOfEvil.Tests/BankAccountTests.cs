22aac946dcc407e3700f84cae12183681bd04b0a
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-21 13:59:45 +0100

    Rename + Bedingungen

---
+++ BankOfEvil/BankOfEvil.Tests/BankAccountTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankOfEvil.Tests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        /* Kontostand abfragen
+         * Betrag einzahlen
+         * -> nicht negativ
+         * Betrag abheben
+         * -> nicht negativ
+         * -> darf nicht unter 0
+         * Neues Konto hat standardmäßig einen Kontostand von 0
+         */ 
+
+    }
+}
--- BankOfEvil/BankOfEvil/BankAccount.cs
+++ BankOfEvil/BankOfEvil/BankAccount.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankOfEvil
 {
-    public class Bankkonto
+    public class BankAccount
     {
     }
 }
