69a411120c54be81bf89465536b5d219f9c12902
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-21 13:45:45 +0100

    Person + Tests

---
+++ Taschenrechner/Taschenrechner.Tests.XUnit/PersonTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Taschenrechner.Tests.XUnit
+{
+    public class PersonTests
+    {
+        /* Anforderungen:
+         * Referenzvergleich
+         * Wertevergleich falls Referenzvergleich fehlschlägt
+         */
+
+        [Fact]
+        [Trait("Personentests","")]
+        public void Person_Equals_Parameter_is_null_Throws_ArgumentNullException()
+        {
+            var p1 = new Person();
+            Assert.Throws<ArgumentNullException>(() => p1.Equals(null));
+        }
+
+        [Theory]
+        [Trait("Personentests","")]
+        [InlineData("wrong string")]
+        [InlineData(12.5)]
+        [InlineData(false)]
+        public void Person_Equals_Parameter_has_wrong_type_returns_false(object wrongInput)
+        {
+            var p1 = new Person();
+            Assert.False(p1.Equals(wrongInput));
+        }
+
+        [Fact]
+        [Trait("Personentests","")]
+        public void Person_Equals_Parameter_Has_Same_Referenece_returns_true()
+        {
+            var p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            var p2 = p1; // Referenzkopie
+
+            Assert.True(p1.Equals(p2));
+        }
+        [Fact]
+        [Trait("Personentests","")]
+        public void Person_Equals_Parameter_Has_Same_Values_returns_true()
+        {
+            var p1 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+            var p2 = new Person { Vorname = "Tom", Nachname = "Ate", Alter = 10, Kontostand = 100 };
+
+            Assert.True(p1.Equals(p2));
+        }
+    }
+}
---
+++ Taschenrechner/Taschenrechner/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taschenrechner
+{
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            // Anforderungen:
+            /* Gleicher Typ ?
+             * Eines null ?
+             * Referenzvergleich
+             * Wertevergleich falls Referenzvergleich fehlschlägt
+             */
+
+            if (obj is null)
+                throw new ArgumentNullException();
+
+            if (obj is Person p) // obj.GetType() == typeof(Person)
+            {
+                //Referenzvergleich
+                if (obj == this)
+                    return true;
+                else // Referenzen nicht gleich, stattdessen werte vergleichen
+                {   // "Hässliche" Variante ohne GetHashcode();
+                    return Vorname.Equals(p.Vorname) &&
+                           Nachname.Equals(p.Nachname) &&
+                           Alter.Equals(p.Alter) &&
+                           Kontostand.Equals(p.Kontostand);
+                }
+            }
+            else
+                return false;
+        }
+    }
+}
