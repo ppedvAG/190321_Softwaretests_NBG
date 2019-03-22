d180f345ff76ef21b040cfcfd82c1f45b382f38f
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-22 11:30:38 +0100

    IRepository erstellt

---
+++ ProjectSelma/ppedv.ProjectSelma.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.ProjectSelma.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        T GetByID<T>(int ID) where T : Entity;
+        void Add<T>(T item) where T : Entity;
+        void Update<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Save();
+    }
+}
