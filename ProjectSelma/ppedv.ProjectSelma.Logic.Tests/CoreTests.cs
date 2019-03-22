3d4b0aae79bd176bc7b809cbca7cd6b2af570b24
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-22 11:41:01 +0100

    Problemstellung: hier müsste man im UnitTest die EF-Implementierung hineingeben

---
+++ ProjectSelma/ppedv.ProjectSelma.Logic.Tests/CoreTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ppedv.ProjectSelma.Logic.Tests
+{
+    public class CoreTests
+    {
+        [Fact]
+        public void Core_GetAllPeople_Get_Exactly_5_People()
+        {
+            var core = new Core(null); // hab kein EF/Azure etc...
+
+            var result = core.GetAllPeople();
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
--- ProjectSelma/ppedv.ProjectSelma.Logic/Core.cs
+++ ProjectSelma/ppedv.ProjectSelma.Logic/Core.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using ppedv.ProjectSelma.Domain;
+using ppedv.ProjectSelma.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace ppedv.ProjectSelma.Logic
 {
     public class Core
     {
+        public Core(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        private readonly IRepository repository;// EF oder Azure oder .xml ....
+
+        public IEnumerable<Person> GetAllPeople()
+        {
+            return repository.GetAll<Person>();
+        }
     }
 }
