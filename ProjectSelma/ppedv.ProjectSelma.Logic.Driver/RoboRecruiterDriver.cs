0bd24aa7bc491fa4cb40e9d2734e2fcc18eb3b93
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-22 12:06:26 +0100

    "Treiber" v1

--- ProjectSelma/ppedv.ProjectSelma.Logic.Driver/RoboRecruiterDriver.cs
+++ ProjectSelma/ppedv.ProjectSelma.Logic.Driver/RoboRecruiterDriver.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using ppedv.ProjectSelma.Domain;
+using ppedv.ProjectSelma.Hardware.RoboRecruiter;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ppedv.ProjectSelma.Logic.Driver
 {
     public class RoboRecruiterDriver
     {
+        public RoboRecruiterDriver()
+        {
+            machine = new XingRecruiter3000();
+        }
+        private readonly XingRecruiter3000 machine;
+
+        public IEnumerable<Person> RecruitPersons(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                yield return machine.RecruitPersonFromXING(); // "Treiber" ruft Funktionalität der "Maschine" auf
+            }
+        }
     }
 }
