799fe3da91414a06420cd33f6635ade3b1c0310d
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-22 16:13:49 +0100

    Kleiner Performancetest mit For und Parallel.For

--- Messen/Messen/Program.cs
+++ Messen/Messen/Program.cs
@@ -10,6 +10,29 @@ namespace Messen
     {
         static void Main(string[] args)
         {
+            Test schleifenTest = new Test(DemoSynchron, DemoTask);
+
+            Console.WriteLine(schleifenTest.RunTestForAllMethods());
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+
+        private static void DemoSynchron()
+        {
+            double[] values = new double[100_000_000];
+            for (int i = 0; i < 100_000_000; i++)
+            {
+                values[i] = i * Math.Pow(i, 0.33333333) - Math.Sin(Math.PI * i);
+            }
+        }
+        private static void DemoTask()
+        {
+            double[] values = new double[100_000_000];
+            Parallel.For(0, 100_000_000, i =>
+              {
+                  values[i] = i * Math.Pow(i, 0.33333333) - Math.Sin(Math.PI * i);
+              });
         }
     }
 }
---
+++ Messen/Messen/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messen
+{
+    class Test
+    {
+        public Test(params Action[] methodsToTest)
+        {
+            this.methodsToTest = methodsToTest;
+            stopwatch = new Stopwatch();
+            log = new StringBuilder();
+            AppDomain.MonitoringIsEnabled = true;
+        }
+        private readonly Action[] methodsToTest;
+        private readonly Stopwatch stopwatch;
+        private readonly StringBuilder log;
+
+        public string RunTestForAllMethods()
+        {
+            log.Clear();
+            foreach (Action method in methodsToTest)
+            {
+                GC.Collect();
+                GC.WaitForFullGCComplete();
+
+                stopwatch.Restart();
+                method?.Invoke();
+                stopwatch.Stop();
+                log.AppendLine("----------------------------------------------------------------------");
+                log.AppendLine($"[{method.Method.Name}]: Time: {stopwatch.ElapsedMilliseconds}ms");
+                log.AppendLine($"[{method.Method.Name}]: Allocated: {AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize / 1024:#,#} kb");
+                log.AppendLine($"[{method.Method.Name}]: Peak Working Set: {Process.GetCurrentProcess().PeakWorkingSet64 / 1024:#,#} kb");
+                log.AppendLine($"[{method.Method.Name}]: GC total memory: {GC.GetTotalMemory(false)} bytes");
+                log.AppendLine($"[{method.Method.Name}]: GC collections in Gen 0: {GC.CollectionCount(0)}");
+                log.AppendLine($"[{method.Method.Name}]: GC collections in Gen 1: {GC.CollectionCount(1)}");
+                log.AppendLine($"[{method.Method.Name}]: GC collections in Gen 2: {GC.CollectionCount(2)}");
+            }
+            return log.ToString();
+        }
+    }
+}
